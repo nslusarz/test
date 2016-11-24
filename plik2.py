b4d7a5f97c9513ff3075ad30cc2a1b89ecc2a5b0
patrycjabru <patrycja.bruzdzinska@gmail.com>
2016-11-24 15:42:55 +0100

    Commit 2

---
+++ plik2.py
@@ -0,0 +1,10 @@
+data = [10, 3, 7, 8, 1, 6, 2, 9, 4, 5, 0]
+
+def sort(data):
+    for i in range(len(data) - 1, 0, -1):
+        for j in range(i):
+            if data[j] > data[j + 1]:
+                data[j], data[j + 1] = data[j + 1], data[j]
+
+sort(data)
+print(data)
